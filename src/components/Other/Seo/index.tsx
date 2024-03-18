417be8ff91f21a543f0e0b55d2501f9ef91bbd47
Anderson Lima <68432098+Andersonlima1@users.noreply.github.com>
2024-03-18 12:08:26 -0300

    Update index.tsx

--- src/components/Other/Seo/index.tsx
+++ src/components/Other/Seo/index.tsx
@@ -1,1 +1,13 @@
+import Head from "next/head";
+import { SeoInterface } from "@/interfaces/SeoInterface";
 
+const Seo = ({ title, description }: SeoInterface) => {
+    return (
+        <Head>
+           
+            <meta name="googlebot" content="index, follow" />
+        </Head>
+    );
+};
+
+export default Seo;
