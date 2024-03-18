805411773860faa80faa5c8ca2e9ec4e3e6a569b
Andersonlima1 <anderson.lima@webera.com>
2024-03-18 09:15:40 -0300

    portfolio has been created

---
+++ src/data/social.tsx
@@ -0,0 +1,10 @@
+import { RiLinkedinFill, RiGithubFill, RiInstagramFill } from "react-icons/ri";
+
+export const icons = [
+    {
+      path: "https://www.linkedin.com/in/anderson-henrique-de-lima-8515631a4/",
+      name: <RiLinkedinFill />,
+      title: "Linkedin"
+    },
+   
+  ];
